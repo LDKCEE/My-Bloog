502bd167ea566396bbe88b2114124057d40154f4
Ayoub Elimrany <ayyoubelimrany015@gmail.com>
2024-07-01 11:23:20 +0200

    hacer funtion de comprobar los usuarios con sus contraseñas

--- auth.js
+++ auth.js
@@ -1,40 +1,47 @@
+//manejar los  parametros de la URL para autentacion 
+const queryParametros = new URLSearchParams(window.location.search);
+const email = queryParametros.get('email');
+const password = queryParametros.get('password');
+
+console.log(`email:${email}`);
+console.log(`contraseña:${password}`);
+
+//Verificar se el usuario ya esta autenticado(verificado)
+if (localStorage.getItem('estaAutenticado') === 'true') { window.location.href = 'blog.html' };
+
+
 // ruta al archivo JSON
 const rutaJSON = 'user.json';
 //finction para leer el archivom JSON
 fetch(rutaJSON)
     .then(response => {
         if (!response.ok) {
-           throw new Error('Error al carga el archivo JSON');
+            throw new Error('Error al carga el archivo JSON');
         }
         return response.json();
     })
-.then(datos => {
-    console.log(datos)
-    //aceder a los datos
-    const usuario = datos.usuario;
-    const contraseña = datos.contraseña;
-    //aqui  puodems utilixar las variables usuario y contraseña
-    console.log(usuario, contraseña)
-})
-.catch(error => {
-    console.error('hubo un problema con la operacion fetch.', error)
-})
-//difinir un objeto con usuario y su clave
-const user = { 'frontend@gmai.com': '1234', };
-//Verificar se el usuario ya esta autenticado(verificado)
-if (localStorage.getItem('estaAutenticado') === 'true') { window.location.href = 'blog.html' };
-//manejar los  parametros de la URL para autentacion 
-const queryParametros = new URLSearchParams(window.location.search);
-const email = queryParametros.get('email');
-const password = queryParametros.get('password');
-//SI hay valores de correo y contraseña en el URL
-if (email && password) {
-    if (user[email] && user[email] === password) {
-        localStorage.setItem('estaAutenticado', 'true');
-        window.location.href = 'blog.html';
-    } else {
-        localStorage.removeItem('estaAutenticado');
-        alert('Por favor vuelve a intentarlo de nuevo')
-        window.location.href = 'login.html';
-    }
-}
+    .then(datos => {
+        console.log(datos);
+        //Verificamos si el usaurio y la contraseña existen en los datos del JSON
+        const usuarioEncontrado = datos.find(usuario => usuario.usuario === email && usuario.contraseña === password);
+
+        if (usuarioEncontrado) {
+                localStorage.setItem('estaAutenticado', 'true');
+            alert(`Usuario ${email} HAS SIDO RECONOCIDO.`);
+            window.location.href = 'blog.html';
+
+        } else {
+            localStorage.removeItem('estaAutenticado');
+            alert(`Usuario ${email} NO ENCONTARDO.`);
+            window.location.href = 'login.html';
+        }
+
+
+    })
+
+    .catch(error => {
+        console.error('hubo un problema con la operacion fetch.', error);
+    });
+
+
+
